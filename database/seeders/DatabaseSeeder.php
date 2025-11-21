ad9d29f93f44b106b32d395e2be3148731535ab1
Nadofullstack <nadegedjossou299@gmail.com>
2025-11-21 17:32:10 +0100

    feat:auth roles setup -install Laravel Breeze -Install spatie Permission -Configure User model with HasRoles -Seed database with Admin and Users rôles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +16,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        //1.Création des roles
+$adminRole = Role::create(['name'=>'admin']);
+$userRole = Role::create(['name'=>'user']);
+        //2.Crétion de l'administrateur
+  $admin = User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@highfive.com',
+            'password' => bcrypt('password'),
+  ]);
+  $admin->assignRole($adminRole);//Attribution du rôle d'administrateur
+
+        //3.Création d'un utilisateur standard pour tester
+      $user=  User::factory(10)->create([
+            'name'=>'Alice Doe',
+            'email'=> 'nad@highfive.com',
+            'password'=>bcrypt('password'),
         ]);
+        $user->assignRole($userRole);//Attribution du rôle d'utilisateur standard
+
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
